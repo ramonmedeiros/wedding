423f2ed4fec5789a11760cbca64eaa3733e78eea
Ramon Medeiros <ramon.rnm@gmail.com>
2025-06-15 09:25:17 +0200

    fix: improve navbar centralizing

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -23,12 +23,13 @@ const Navbar = () => {
       className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${isScrolled ? "bg-wedding-white/90 backdrop-blur-md py-3 shadow-sm" : "bg-transparent py-6"
         }`}
     >
-      <div className="flex items-center">
-        <a href="/" className="hidden md:inline-block md:ml-10 text-wedding-darkgray md:text-xl font-normal">
-          Kübra & Ramon
-        </a>
-
-        <div className="flex items-center space-x-8 mx-auto text-shadow-lg/80">
+      <div className="container mx-auto px-4 h-full flex items-center justify-center relative">
+        <div className="absolute left-4 top-1/2 transform -translate-y-1/2">
+          <a href="/" className="hidden md:inline-block text-wedding-darkgray md:text-xl font-normal">
+            Kübra & Ramon
+          </a>
+        </div>
+        <div className="flex items-center space-x-8 text-shadow-lg/80">
           <NavLink to="/" current={location.pathname === "/"}>
             {t("home")}
           </NavLink>
