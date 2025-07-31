328dbe0c42c3281a8a23bf59c789b32f2ce93219
Ramon Medeiros <ramon.rnm@gmail.com>
2025-07-31 08:28:19 +0200

    fix: add new event page

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,99 +1,94 @@
 import Layout from "@/components/layout/Layout";
 import Title from "@/components/layout/Title";
-import { TooltipProvider } from "@radix-ui/react-tooltip";
-import { ChevronDownIcon } from "lucide-react";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
 import { Accordion } from "radix-ui";
 import { useTranslation } from "react-i18next";
 
+const events = [
+    {
+      time: "15-20 February",
+      title: "Rio de Janeiro - Carnaval",
+      anchor: "rio",
+      description: `
+          To kick things off, Ramon and I are heading to Rio for Carnaval, and we’re turning it into a pre-wedding celebration! 🎭🥳
+
+          Carnaval officially begins on Feb 14, but we’ll be arriving on the night of the 15th and staying until the 20th, based at Hotel ibis Copacabana Posto 2.
+
+          Some awesome friends are already in and we’d LOVE for more of you to join! Expect beach days, bloco parties, caipirinhas, and maybe even a night at the Sambódromo..
+
+          Important: Hotels are booking up fast, so we recommend reserving your accommodations as soon as possible!
+
+          If you want a more relaxed vibe after the Carnaval craziness, we definitely suggest staying a few extra days to enjoy some calmer beach time or explore other parts of Brazil — it’s the perfect way to recharge! 🌴🌞
+
+          Rough plan:
+          * Feb 16–19 – blocos, beach, street parties, and general good chaos
+          * Feb 20 – farewell breakfast near the hotel
+
+          We seriously can’t wait to celebrate with you all and this is just the beginning! 💕
+          Let’s make it unforgettable! 🌞💃🕺
+          `
+    },
+    {
+      time: "21-24 February",
+      title: "Our family time",
+      anchor: "family-time",
+      description: "We will keep with our parents the week before"
+    },
+    {
+      time: "25 February",
+      title: "Pre-wedding",
+      anchor: "pre-wedding",
+      description: "Let's meet 2 days before the wedding, so we can meet each other and get the party started"
+    },
+  ];
+
 export default function Schedule() {
-  const events = [
-    {
-      time: "14-18 February",
-      title: "Rio de Janeiro - Carnaval ",
-      image: "/images/christ-the-redeemer.svg",
-      description: "We're kicking off with an unforgetable adventure. Carnaval in Rio! Get ready for vibrant parades, samba in the streets, and elecdtric atmosphere you'll never forget. Whether you're a Carnaval pro or just curios to experince the magic, we'd love for you to join us.",
-      side: "left",
-    },
-    {
-      time: "4:00 PM",
-      title: "Guest Arrival",
-      description: "Guests are welcome to arrive and be seated in the garden.",
-      image: "/images/christ-the-redeemer.svg",
-      side: "right",
-    },
-    {
-      time: "4:00 PM",
-      title: "Guest Arrival",
-      description: "Guests are welcome to arrive and be seated in the garden.",
-      image: "/images/christ-the-redeemer.svg",
-      side: "left",
-    },
-  ]
   const { t } = useTranslation();
 
   return (
     <Layout>
       <Title title={t("schedule")} description={t("schedule_description")} />
-      <TooltipProvider>
-        <div className="relative w-full max-w-4xl mx-auto">
-          {/* Central Vertical Line */}
-          <div className="absolute left-1/2 transform -translate-x-1/2 h-full w-px bg-black rounded-full" />
-
-          <div className="space-y-12">
-            {events.map((event, index) => {
-              const isLeft = event.side === "left" || (!event.side && index % 2 === 0);
-
-              return (
-                <div
-                  key={index}
-                  className={`relative flex items-center ${isLeft ? "justify-start" : "justify-end"}`}
-                >
-                  <div className={`w-1/2 px-4`}>
-                    {createDropDown(index, event)}
-                  </div>
-
-                  {/* Dot on the central line */}
-                  <div className="absolute left-1/2 transform -translate-x-1/2 w-2 h-2 bg-black rounded-full" />
-                </div>
-              );
-            })}
+      <div className="relative mx-auto w-full max-w-4xl flex justify-around items-center">
+        {/* Horizontal Line */}
+        <div className="absolute top-10 left-0 right-0 h-0.5 -translate-y-1/2 bg-gray-300 -z-10" />
+        {events.map((event, index) => (
+          <div key={index} className="flex flex-col items-center text-center">
+            <div className="mb-2 bg-white px-2">
+              <a className="text-lg font-semibold" href={`#${event.anchor}`}>{event.title}</a>
+            </div>
+            <div className="w-2 h-2 rounded-full bg-black" />
+            <div className="mt-2 bg-white px-2">
+              <p className="text-sm text-gray-500">{event.time}</p>
+            </div>
           </div>
-        </div>
-      </TooltipProvider>
+        ))}
+      </div>
+
+      <div className="container mx-auto mt-20 md:px-80 fade-in-delay-2">
+        <Accordion.Root type="multiple" className="w-10% space-y-3">
+          {events.map((item, index) => (
+            <Accordion.Item
+              key={index}
+              value={index.toString()}
+              className="overflow-hidden rounded-md border border-wedding-gray/30 bg-white shadow-sm transition-all focus-within:ring-2 focus-within:ring-wedding-darkgray/50"
+            >
+              <Accordion.Header className="flex">
+                <Accordion.Trigger className="group flex w-full flex-1 cursor-pointer items-center justify-between p-4 text-left text-wedding-darkgray hover:bg-wedding-offwhite/50">
+                  <span className="text-base font-medium md:text-base">
+                    {`${index + 1}. ${item.title}`}
+                  </span>
+                  <ChevronDownIcon className="h-5 w-5 transform text-wedding-darkgray transition-transform duration-300 ease-in-out group-data-[state=open]:rotate-180" />
+                </Accordion.Trigger>
+              </Accordion.Header>
+              <Accordion.Content className="overflow-hidden text-sm text-wedding-gray data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp">
+                <div className="px-4 pb-4 pt-2">
+                  {item.description}
+                </div>
+              </Accordion.Content>
+            </Accordion.Item>
+          ))}
+        </Accordion.Root>
+      </div>
     </Layout>
   );
 }
-
-const createDropDown = (index, event) => {
-  const justifyContent = event.side === "right" ? "justify-items-start" : "justify-items-end";
-
-  return (
-    <Accordion.Root
-      type="multiple"
-      className="w-10% space-y-3"
-    >
-      <Accordion.Item
-        key={index}
-        value={index.toString()}
-        className="overflow-hidden rounded-md transition-all"
-      >
-        <Accordion.Header className="flex">
-          <Accordion.Trigger className="grid grid-cols-6 group flex w-full flex-1 cursor-pointer items-center justify-between pl-4 pt-4 pr-4 pb-2 text-left text-wedding-darkgray">
-            {event.side === "right" && <img className="col-span-1" src={event.image} />}
-            <div className={`bg-white rounded-2xl p-4 col-span-5 ${justifyContent}`}>
-              <h3 className="text-lg font-semibold col-span-3">{event.title}</h3>
-              <p className="text-sm text-gray-500">{event.time}</p>
-              <ChevronDownIcon className="col-span-1 h-5 w-5 transform text-wedding-darkgray transition-transform duration-300 ease-in-out group-data-[state=open]:rotate-180" />
-            </div>
-            {event.side === "left" && <img className="col-span-1" src={event.image} />}
-          </Accordion.Trigger>
-        </Accordion.Header>
-        <Accordion.Content className="overflow-hidden text-sm text-wedding-gray data-[state=open]:animate-slideDown data-[state=closed]:animate-slideUp">
-          <div className="px-4 pb-4">
-            <p className="text-sm text-gray-700">{event.description}</p>
-          </div>
-        </Accordion.Content>
-      </Accordion.Item>
-    </Accordion.Root>
-  )
-} 
