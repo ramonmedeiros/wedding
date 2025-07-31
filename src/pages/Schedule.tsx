cf851f5276b92b0d53c54f3f4442b7894ffd4ee4
Ramon Medeiros <ramon.rnm@gmail.com>
2025-07-31 22:07:56 +0200

    fix: add tests and formatting

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -4,21 +4,23 @@ import { useTranslation } from "react-i18next";
 const events = [
   {
     time: "15-20 February",
-    title: "Rio de Janeiro",
+    title: "Rio - Carnaval",
     anchor: "rio",
-    description: "To kick things off, Ramon and I are heading to Rio for Carnaval, and we’re turning it into a pre-wedding celebration! 🎭🥳\nCarnaval officially begins on Feb 14, but we’ll be arriving on the night of the 15th and staying until the 20th, based at Hotel ibis Copacabana Posto 2.\nSome awesome friends are already in and we’d LOVE for more of you to join! Expect beach days, bloco parties, caipirinhas, and maybe even a night at the Sambódromo.\n\nImportant: Hotels are booking up fast, so we recommend reserving your accommodations as soon as possible!\n\nIf you want a more relaxed vibe after the Carnaval craziness, we definitely suggest staying a few extra days to enjoy some calmer beach time or explore other parts of Brazil — it’s the perfect way to recharge! 🌴🌞\n\nRough plan:\n* Feb 16–19 – blocos, beach, street parties, and general good chaos\n* Feb 20 – farewell breakfast near the hotel\n\nWe seriously can’t wait to celebrate with you all and this is just the beginning! 💕\nLet’s make it unforgettable! 🌞💃🕺\n"
+    location: "Copacabana & Ipanema (see map)",
+    description: "We’re kicking off our wedding vacation with one of the world’s most iconic celebrations—Carnival in Rio!\nJoin us for sun, samba, and unforgettable moments as we dive into the heart of Brazilian culture.\n\nCarnival officially begins on February 14, but we’ll be arriving the night of February 15 and staying until February 20, based in Copacabana at the Hotel ibis Copacabana Posto 2—right by the beach.\n\nWhile most of the biggest Carnival parties and blocos happen in Downtown Rio (Centro), getting there is easy—Uber is safe, reliable, and affordable, so you’ll be able to go back and forth without hassle.\n\nExpect beach days, bloco parties (street parades), flowing caipirinhas, and maybe even a night at the legendary Sambódromo.🌴🍹🌞\n\n✨ What to Expect:\nFeb 16–19 – Blocos, beach, street parties, and beautiful chaos\nFeb 19 – Group dinner to unwind and catch up\n\nCome for the dancing, stay for the memories. Whether you're down for the full Carnival experience or just want to enjoy the vibes by the beach, we can't wait to celebrate with you. 🌞💃🕺",
   },
   {
     time: "21-25 February",
-    title: "Our family time",
+    title: "Family time",
     anchor: "family-time",
-    description: "We will keep with our parents the week before. Unfortunately we will not be available to enjoy with you guys."
+    description: "After the high energy of Carnival, we’re setting aside some quiet time for our families to connect before the big day.\nSince many of our loved ones haven’t had the chance to meet yet, we’ll be spending a few days in Campinas with both sides of the family.\n\nThis time is especially important to us, and we’re so excited to see our families come together.\n\nWe’ll be off the grid (just a bit!) during this period, focusing on family time before the celebrations continue.\n That said, we’ll still be keeping an eye on the group chats and, if the opportunity arises, we might join you for a spontaneous hangout or two!"
   },
   {
     time: "26 February",
     title: "Pre-wedding",
     anchor: "pre-wedding",
-    description: "Let's meet 1 day before the wedding, have a meal and some drinks together, so we can kickoff the party mode"
+    location: "Campinas - São Paulo (see map)",
+    description: "Details coming soon. Stay tuned"
   },
 ];
 
@@ -52,17 +54,12 @@ export default function Schedule() {
 
       <div className="container mx-auto mt-20 md:px-80 fade-in-delay-2">
         {events.map((item, index) => (
-          <div key={index} id={item.anchor}>
-            <h1 className="group flex w-full flex-1 items-center justify-between p-4 text-left text-wedding-darkgray hover:bg-wedding-offwhite/50">
-              <span className="text-base font-medium md:text-base">{item.title}</span>
-            </h1>
-            <h2 className="px-4 pb-4 pt-2 overflow-hidden text-wedding-gray text-sm">
-              {item.description.split('\n').map((line, i) => (
-                    <p key={i}>
-                      {line}
-                    </p>
-                  ))}
-            </h2>
+          <div className="font-semibold text-wedding-darkgray pb-5" key={index} id={item.anchor}>
+            <span>
+              <h3 className="text-xl font-semibold">{item.title}</h3>
+            </span>
+            {item.location && <h4 className="pt-2 pb-2 font-semibold">Location to stay at: {item.location}</h4>}
+            <span className="wrap-anywhere whitespace-pre-wrap pb-4 pt-2 font-light text-wedding-darkgray text-sm">{item.description}</span>
           </div>
         ))}
       </div>
