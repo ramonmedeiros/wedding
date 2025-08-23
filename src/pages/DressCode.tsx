4b9111694c610a27f55a55c033653d50bc3ff08e
Ramon Medeiros <ramon.rnm@gmail.com>
2025-08-23 14:08:15 +0200

    feat: add translations for dresscode

--- src/pages/DressCode.tsx
+++ src/pages/DressCode.tsx
@@ -8,13 +8,13 @@ const DressCode = () => {
   return (
     <Layout>
       <Title title={t("dresscode")} />
-       <div className="container mx-auto text-center">
+       <div className="container mx-auto text-center fade-in-delay-1">
         <img
           src={"/images/dresscode.png"}
           alt={"/images/dresscode.jpg"}
           className="mx-auto md:h-[50vh] md:w-[110vh] h-[15vh] w-[60vh] object-center md:object-center object-cover md:object-cover"
         />
-        <p className="whitespace-pre-wrap text-wedding-gray text-base max-w-3xl mx-auto fade-in-delay-1">
+        <p className="whitespace-pre-wrap text-wedding-gray text-base max-w-3xl mx-auto">
           {t("dresscode_description")}
         </p>
       </div>
