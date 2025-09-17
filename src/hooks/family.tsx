f35c770f31565800c57953f721fc6de3ecb5b22a
Ramon Medeiros <ramon.rnm@gmail.com>
2025-09-17 20:13:59 +0200

    feat: fix custom message when declining RSVP
    - set different message according to answer

--- src/hooks/family.tsx
+++ src/hooks/family.tsx
@@ -1,56 +1,56 @@
 export const BACKEND_URL = "https://api.kubraandramon.com/"
 
 export const getFamily = (code: string): Promise<Family> => {
-    return fetch(BACKEND_URL + "family/" + code).
-        then(response => response.json()).
-        then(data => data as Family).
-        catch(e => {
-            console.error(e)
-            return Promise.reject(new Error(
-                e?.
-                    map((e: Error) => e.message).
-                    join('\n') ?? 'unknown',
-            ))
-        })
+  return fetch(BACKEND_URL + "family/" + code).
+    then(response => response.json()).
+    then(data => data as Family).
+    catch(e => {
+      console.error(e)
+      return Promise.reject(new Error(
+        e?.
+          map((e: Error) => e.message).
+          join('\n') ?? 'unknown',
+      ))
+    })
 }
 
 export const updateFamily = (code: string, confirmed_guests: string[], confirmation: boolean, comments: string, songs: string[], alergies: Alergy[]) => {
-    const url = new URL(BACKEND_URL + "family/" + code);
+  const url = new URL(BACKEND_URL + "family/" + code);
 
-    return fetch(url.toString(), {
-        method: "POST",
-        body: JSON.stringify(
-            {
-                confirmed_guests: confirmed_guests,
-                confirmed: confirmation,
-                comments: comments,
-                songs: songs,
-                alergies: alergies
-            }
-        ),
-    }).
-        catch(e => {
-            console.error(e)
-            return Promise.reject(new Error(
-                e?.
-                    map((e: Error) => e.message).
-                    join('\n') ?? 'unknown',
-            ))
-        })
+  return fetch(url.toString(), {
+    method: "POST",
+    body: JSON.stringify(
+      {
+        confirmed_guests: confirmed_guests,
+        confirmed: confirmation,
+        comments: comments,
+        songs: songs,
+        alergies: alergies
+      }
+    ),
+  }).
+    catch(e => {
+      console.error(e)
+      return Promise.reject(new Error(
+        e?.
+          map((e: Error) => e.message).
+          join('\n') ?? 'unknown',
+      ))
+    })
 }
 
 export interface Alergy {
-    id: string
-    name: string
-    count: number
+  id: string
+  name: string
+  count: number
 }
 
 export interface Family {
-    expected_guests: string[]
-    confirmed_guests?: string[]
-    comments: string
-    confirmed: boolean
-    songs: string[]
-    alergies: Alergy[]
-    confirmed_at?: Date
+  expected_guests: string[]
+  confirmed_guests?: string[]
+  comments: string
+  confirmed: boolean
+  songs: string[]
+  alergies: Alergy[]
+  confirmed_at?: Date
 }
